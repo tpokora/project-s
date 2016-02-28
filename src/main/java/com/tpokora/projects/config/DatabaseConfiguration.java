b7d987dfb94b5d32a073d2e02e49f86ae8302626
tpokora <pokora.tomek@gmail.com>
2016-02-28 12:48:13 +0100

    Hibernate configuration

---
+++ src/main/java/com/tpokora/projects/config/DatabaseConfiguration.java
@@ -0,0 +1,71 @@
+package com.tpokora.projects.config;
+
+/**
+ * Created by pokor on 28.02.2016.
+ */
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:properties/developer.properties")
+public class DatabaseConfiguration {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("database.driver"));
+        dataSource.setUrl(env.getProperty("database.url"));
+        dataSource.setUsername(env.getProperty("database.login"));
+        dataSource.setPassword(env.getProperty("database.password"));
+
+        return dataSource;
+    }
+
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+        properties.put("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        return properties;
+    }
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) {
+
+        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+        sessionBuilder.addProperties(getHibernateProperties());
+        // Object.class change to real data Model class after comas
+        // ex. ModelObject1.class, ModelObject2.class
+        //sessionBuilder.addAnnotatedClasses(User.class);
+
+        return sessionBuilder.buildSessionFactory();
+    }
+
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(
+            SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+                sessionFactory);
+
+        return transactionManager;
+    }
+
+}
