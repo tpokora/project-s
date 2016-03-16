4233da8a7872fb1cc34dc9e0b2fe5e0a44dc1b6e
tpokora <pokora.tomek@gmail.com>
2016-03-16 22:33:31 +0100

    TableDetails model basics

---
+++ src/main/java/com/tpokora/projects/common/model/TableDescriber.java
@@ -0,0 +1,8 @@
+package com.tpokora.projects.common.model;
+
+/**
+ * Created by pokor on 16.03.2016.
+ */
+public interface TableDescriber {
+    public void setTableDescription(Class clazz);
+}
---
+++ src/main/java/com/tpokora/projects/common/model/TableDetails.java
@@ -0,0 +1,41 @@
+package com.tpokora.projects.common.model;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by pokor on 16.03.2016.
+ */
+public class TableDetails implements TableDescriber {
+    private String tableName;
+    private Map<String, String> tableColumnsDescription = new HashMap<String, String>();
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public TableDetails() {}
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableColumnsDescription(HashMap<String, String> tableColumnsDescription) {
+        this.tableColumnsDescription = tableColumnsDescription;
+    }
+
+    public Map<String, String> getTableColumnsDescription() {
+        return tableColumnsDescription;
+    }
+
+    @Override
+    public void setTableDescription(Class clazz) {
+        // TODO: set table details
+    }
+}
