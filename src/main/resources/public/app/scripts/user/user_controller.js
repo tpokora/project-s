89e99ec54172470e82f41d3c4789e6b4c6db9ba8
tpokora <pokora.tomek@gmail.com>
2016-10-23 21:55:38 +0200

    User update password html + password check with error display

--- src/main/resources/public/app/scripts/user/user_controller.js
+++ src/main/resources/public/app/scripts/user/user_controller.js
@@ -26,10 +26,31 @@ App.controller('UsersController', ['$scope', '$stateParams', '$location', 'UserS
             );
     };
 
+    function checkUpdatePasswords(firstPassword, secondPassword) {
+        if (firstPassword == secondPassword) {
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    $scope.updateUser = function() {
+        self.userToUpdate = $scope.user;
+        if ($scope.newPassword != '') {
+            if (checkUpdatePasswords($scope.newPassword, $scope.newPasswordRepeat)) {
+                // Password update
+            } else {
+                $scope.updateError = 'Password not the same';
+            }
+        }
+    };
+
     this.fetchAllUsers();
 
     // Add user based on ID param
     if (params.id != null) {
         this.fetchUserById(params.id);
     }
+
+
 }]);
