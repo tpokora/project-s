ad7481964248e50779c6ad62b9c418940b903965
tpokora <pokora.tomek@gmail.com>
2016-10-26 20:56:08 +0200

    Reset user password scheduler

--- src/main/java/com/tpokora/projects/config/App.java
+++ src/main/java/com/tpokora/projects/config/App.java
@@ -3,12 +3,14 @@ package com.tpokora.projects.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by pokor on 28.02.2016.
  */
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.tpokora.projects" })
+@EnableScheduling
 public class App {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tpokora/projects/scheulder/UserPasswordResetScheduler.java
@@ -0,0 +1,33 @@
+package com.tpokora.projects.scheulder;
+
+import com.tpokora.projects.common.utils.SessionIdentifierGenerator;
+import org.apache.catalina.SessionIdGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by pokor on 26.10.2016.
+ */
+@Component
+public class UserPasswordResetScheduler {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserPasswordResetScheduler.class);
+
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+
+    private static final int USER_PASSWORD_RESET_SCHEDULER = 5000;
+
+    /**
+     * Scheduler checks time of reset password creation and removes session after time
+     */
+    @Scheduled(fixedRate = USER_PASSWORD_RESET_SCHEDULER)
+    public void userResetPasswordScheduler() {
+        logger.info("Current time is {}", SIMPLE_DATE_FORMAT.format(new Date()));
+        // Check DB if sessions of reset password are overdue
+    }
+}
