04966b2381c0c76ffde7b6f64a16cc804ad954c3
tpokora <pokora.tomek@gmail.com>
2017-01-26 21:10:41 +0100

    Add @Repository to UserPasswordRepository

--- src/main/java/com/tpokora/projects/user/dao/UserPasswordRepository.java
+++ src/main/java/com/tpokora/projects/user/dao/UserPasswordRepository.java
@@ -2,9 +2,11 @@ package com.tpokora.projects.user.dao;
 
 import com.tpokora.projects.user.model.UserPassword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by pokor on 01.11.2016.
  */
+@Repository
 public interface UserPasswordRepository extends JpaRepository<UserPassword, Integer> {
 }
